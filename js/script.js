646805921f462735ce6d1225feda0723f7dffbc5
lppgn <33622086+lppgn@users.noreply.github.com>
2020-04-11 16:55:59 +0200

    Början på min pokemon sida

--- js/script.js
+++ js/script.js
@@ -1,76 +1,8 @@
-//array
-var gameBoardArray = new Array(10);
-for (let index = 0; index < gameBoardArray.length; index++) {
-    gameBoardArray[index] = new Array(10);
-}
-
-let allGridPositions = document.querySelectorAll(".gameBlock");
-console.table(gameBoardArray);
-for (let X = 0; X < gameBoardArray.length; X++) {
-    for (let Y = 0; Y < gameBoardArray.length; Y++) {
-        gameBoardArray[X][Y] = allGridPositions[(Y*10)+X]
-    }    
-}
-console.table(gameBoardArray);
-
-//location
-let currentX = 4;
-let currentY = 4;
-
-//movement
-let xMovement = 0;
-let yMovement = 0;
-update();
-let headlocation = gameBoardArray[currentX][currentY];
-headlocation.style.background = "green";
-function clickFunction() { 
-    if(event.keyCode == 37 && xMovement != 1) {
-        xMovement = -1;
-        yMovement = 0;
-    }
-    else if(event.keyCode == 39 && xMovement != -1) {
-        xMovement = 1;
-        yMovement = 0;
-    }
-    else if(event.keyCode == 38 && yMovement != 1) {
-        yMovement = -1;
-        xMovement = 0;
-    }
-    else if(event.keyCode == 40 && yMovement != -1) {
-        yMovement = 1;
-        xMovement = 0;
+let pokeArray = [document.getElementById("0"), document.getElementById("1"), document.getElementById("2"), document.getElementById("3")];
+console.table(pokeArray);
+let currentDisplay = 0;
+for (let index = 0; index < pokeArray.length; index++) {
+    if(index != currentDisplay){
+        pokeArray[index].style.display = "none";
     }
 }
-function update(progress) {
-    // Update the state of the world for the elapsed time since last render
-    progress = 1.67;
-    //head movement
-    var timer = setTimeout(moveFunction, 2000);
-    function moveFunction() {
-    let headlocation = gameBoardArray[currentX][currentY];
-    headlocation.style.background = "green";
-    document.addEventListener('keydown', clickFunction);
-    currentX = currentX + (xMovement);
-    currentY = currentY + (yMovement);
-    }
-  }
-  
-  function draw() {
-}
-  
-  function loop(timestamp) {
-    var progress = timestamp - lastRender;
-  
-    update(progress);
-    draw();
-  
-    lastRender = timestamp;
-    window.requestAnimationFrame(loop);
-  }
-  var lastRender = 0;
-  window.requestAnimationFrame(loop);
-
-
-//tail movement
-
-//äpplen
