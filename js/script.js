31d56b69fd6da600faea678a004b89a1e418873b
lppgn <33622086+lppgn@users.noreply.github.com>
2020-04-13 15:28:19 +0200

    Färdigt projekt
    Jag uppdaterade inte så mycket på git utan jobbade bara färdigt istället.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,59 @@
 let pokeArray = [document.getElementById("0"), document.getElementById("1"), document.getElementById("2"), document.getElementById("3")];
-console.table(pokeArray);
 let currentDisplay = 0;
 for (let index = 0; index < pokeArray.length; index++) {
     if(index != currentDisplay){
         pokeArray[index].style.display = "none";
+    }
+}
+
+let buttonArray = [document.getElementById("previous"), document.getElementById("next")];
+if(currentDisplay==0){
+buttonArray[currentDisplay].disabled = true;
+}
+else{
+    buttonArray[0].disabled = false;
+}
+if(currentDisplay == pokeArray.length){
+    buttonArray[1].disabled = true;
+}
+else{
+    buttonArray[1].disabled = false;
+}
+const pageChange = document.querySelectorAll(".changePage");
+for (let index = 0; index < pageChange.length; index++) {
+    pageChange[index].addEventListener("click", changePage);
+}
+const backgroundChange = document.querySelectorAll(".filler");
+function changePage (){
+    if(this.value == -1){
+        currentDisplay -= 1;
+    }
+    else if(this.value == 1){
+        currentDisplay += 1;
+    }
+    console.log(currentDisplay);
+    for (let index = 0; index < pokeArray.length; index++) {
+        if(index != currentDisplay){
+            pokeArray[index].style.display = "none";
+        }
+    }
+    pokeArray[currentDisplay].style.display = "grid";
+    if(currentDisplay==0){
+        buttonArray[currentDisplay].disabled = true;
+    }
+    else{
+            buttonArray[0].disabled = false;
+    }
+    if(currentDisplay == 3){
+        buttonArray[1].disabled = true;
+        for (let index = 0; index < backgroundChange.length; index++) {
+            backgroundChange[index].style.backgroundImage = "url('../img/Fire.png')";
+        }   
+    }
+    else{
+            buttonArray[1].disabled = false;
+            for (let index = 0; index < backgroundChange.length; index++) {
+                backgroundChange[index].style.backgroundImage = "url('../img/Grass.png')";
+            }
     }
 }
